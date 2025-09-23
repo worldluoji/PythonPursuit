5db7725e46da28763e83e40b7245aac083fed173
worldluoji <qq1132416@outlook.com>
2025-09-23 11:06:24 +0800

    fix: change class name to upper tf

--- python-basic/design/Structural-Patterns/decorator/class_decorator_args.py
+++ python-basic/design/Structural-Patterns/decorator/class_decorator_args.py
@@ -1,4 +1,4 @@
-class makeHtmlTagClass(object):
+class MakeHtmlTagClass(object):
  
     def __init__(self, tag, css_class=""):
         self._tag = tag
@@ -16,8 +16,8 @@ class makeHtmlTagClass(object):
 1）如果 decorator 有参数的话，__init__() 成员就不能传入 fn 了，而 fn 是在 __call__ 的时候传入的。
 2）这段代码还展示了 wrapped(*args, **kwargs) 这种方式来传递被 decorator 函数的参数
 '''
-@makeHtmlTagClass(tag="b", css_class="bold_css")
-@makeHtmlTagClass(tag="i", css_class="italic_css")
+@MakeHtmlTagClass(tag="b", css_class="bold_css")
+@MakeHtmlTagClass(tag="i", css_class="italic_css")
 def hello(name):
     return "Hello, {}".format(name)
  
