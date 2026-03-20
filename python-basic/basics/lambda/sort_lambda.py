759593cfe85e109b5f2681801c49f800d1067539
worldluoji <qq1132416@outlook.com>
2026-03-20 14:42:58 +0800

    refactor: prettier sort_lambda demo

--- python-basic/basics/lambda/sort_lambda.py
+++ python-basic/basics/lambda/sort_lambda.py
@@ -1,31 +1,27 @@
+import operator
 
+# Define the list of records
 list_records = [
- ('Financial', '张三', 6000),
- ('IT', '丁一', 8000),
- ('Marketing', '李四', 8000),
- ('IT', '王二', 7000)
+    ('Financial', '张三', 6000),
+    ('IT', '丁一', 8000),
+    ('Marketing', '李四', 8000),
+    ('IT', '王二', 7000)
 ]
 
-# 先基于department排序，然后再在部门内按照salary排序, reverse=True表示降序排列
-list_records.sort(
- key = lambda l: (l[0], l[2]),
- reverse=True
-)
+# Sort the list first by department, then by salary in descending order
+list_records.sort(key=operator.itemgetter(0, 2), reverse=True)
 
 print(list_records)
-print('*' * 20)
 
-
+# Define dictionary of ages
 dictionary_age = {
-   '张三': 30,
-   '丁一': 35,
-   '李四': 18,
-   '王二': 25,
+    '张三': 30,
+    '丁一': 35,
+    '李四': 18,
+    '王二': 25,
 }
 
-# 先基于department排序, 再基于dictionary_age按照年龄排
-list_records.sort(
- key = lambda l:( l[0], dictionary_age[l[1]] )
-)
+# Sort the list by department and then by age in ascending order
+list_records.sort(key=lambda x: (x[0], dictionary_age[x[1]]))
 
 print(list_records)
